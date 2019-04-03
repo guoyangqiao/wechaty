f74d65189e2c023297a6abb9573b6b19872ff83c
guoyangqiao <Gu0yangqiao>
2019-04-03 18:19:55 +0800

    autocommit

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const {Wechaty} = require('wechaty');
+const {FileBox} = require('file-box');
+const qrTerm = require('qrcode-terminal');
+
+//global initialize area
 const bot = new Wechaty();
-bot.on('scan', (qrcode, status) => console.log(['https://api.qrserver.com/v1/create-qr-code/?data=', encodeURIComponent(qrcode), '&size=220x220&margin=20',].join('')));
-bot.on('login', user => console.log(`User ${user} logined`));
-bot.on('message', message => console.log(`Message: ${message}`));
+bot.on('scan', (qrcode, status) => {
+    qrTerm.generate(qrcode, {small: true});
+    const qrcodeImageUrl = [
+        'https://api.qrserver.com/v1/create-qr-code/?data=',
+        encodeURIComponent(qrcode),
+    ].join('');
+    console.log(`[${status}] ${qrcodeImageUrl}\n扫描上面的二维码来登录你的微信`);
+});
+bot.on('login', user => {
+    console.log(`用户 ${user} 登录成功!`);
+    core(user);
+});
+bot.on('logout', (user) => {
+    console.log(`用户 ${user} 退出`);
+});
+bot.on('error', (error) => {
+    console.error(error)
+});
 bot.start();
+
+//functions======================
+function core(user) {
+    var docFile = process.argv[2];
+    var contactFile = process.argv[3];
+    let contact = bot.Contact.load("wiThy3Justc");
+    console.log(contact);
+    // const xlsExample = FileBox.fromFile('/Users/guoyangqiao/Desktop/JVMS.xls');
+    // bot.Contact.find({name: "CLOTGTRYC"}).then(
+    //     (contact) => {
+    //         contact.say(xlsExample);
+    //     });
+}
 
