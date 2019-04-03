fa2cb6b37e98f08180e689add3f453692b47fe04
guoyangqiao <Gu0yangqiao>
2019-04-04 02:49:12 +0800

    autocommit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //npm config set registry https://registry.npm.taobao.org
 //npm config set puppeteer_download_host=https://npm.taobao.org/mirrors
 //git clone https://github.com/guoyangqiao/wechaty.git or other way
+//create log file
 //npm install
 //node index.js ContactList.txt XLS file "words"
 const {Wechaty} = require('wechaty');
