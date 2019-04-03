aa0da73f28d44e594b1fc1e714b9f53f0542a86c
guoyangqiao <Gu0yangqiao>
2019-04-03 19:08:36 +0800

    autocommit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const {Wechaty} = require('wechaty');
 const {FileBox} = require('file-box');
 const qrTerm = require('qrcode-terminal');
+const fs = require('fs');
+const readline = require('readline');
 
 //global initialize area
 const bot = Wechaty.instance({profile: 'autoLogin'});
 bot.on('scan', (qrcode, status) => {
     if (status === 0) {
         qrTerm.generate(qrcode, {small: true});
-        console.log('扫描二维码登录微信');
-    } else {
-        //不做任何操作
+        console.log(status, '扫描二维码登录微信');
     }
 });
 bot.on('login', user => {
@@ -29,11 +29,16 @@ bot.start();
 function core(user) {
     var docFile = process.argv[2];
     var contactFile = process.argv[3];
-    // const xlsExample = FileBox.fromFile('/Users/guoyangqiao/Desktop/JVMS.xls');
-    // bot.Contact.find({name: "CLOTGTRYC"}).then(
-    //     (contact) => {
-    //         contact.say(xlsExample);
-    //     });
+    const xlsExample = FileBox.fromFile(docFile);
+    const lineReader = readline.createInterface({
+        input: fs.createReadStream(contactFile),
+        crlfDelay: Infinity
+    });
+    lineReader.on('line', function (cName) {
+        bot.Contact.find({name: cName}).then(
+            (contact) => {
+                contact.say(xlsExample);
+            });
+    });
     console.log("执行结束");
 }
-
